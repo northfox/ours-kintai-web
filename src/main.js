baf7dd305ae99b7f28119d8172877d5427b018a5
northfox <vulpes.engineer@gmail.com>
2019-12-14 17:06:32 +0900

    Update settings pj

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import axios from 'axios'
+import dayjs from 'dayjs'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.prototype.$dayjs = dayjs
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
