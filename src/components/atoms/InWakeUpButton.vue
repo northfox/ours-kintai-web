4aaaad4b8f7c77d4f2e8213134a9f50ed76332cd
northfox <vulpes.engineer@gmail.com>
2019-12-14 18:58:17 +0900

    Update in wake up @click time to log

--- src/components/atoms/InWakeUpButton.vue
+++ src/components/atoms/InWakeUpButton.vue
@@ -1,9 +1,15 @@
 <template>
-  <button class="btn btn-lg btn-primary">起床</button>
+  <button class="btn btn-lg btn-primary" @click="save">起床</button>
 </template>
 
 <script>
 export default {
-  name: 'in-wake-up-button'
+  name: 'in-wake-up-button',
+  methods: {
+    save() {
+      let now = this.$dayjs().format('YYYY-MM-DDTHH:mm:ss')
+      console.log(now)
+    }
+  }
 }
 </script>
